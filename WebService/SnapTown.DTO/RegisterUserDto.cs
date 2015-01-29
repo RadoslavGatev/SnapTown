f30d792b9be1eab0d90797771c56464b9160b11f
RadoslavGatev <radoslavgatev@gmail.com>
2015-01-29 23:17:03 +0200

    Added Users api - for registering the user and updating his authtoken. Deleted Values controller.

---
+++ WebService/SnapTown.DTO/RegisterUserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapTown.DTO
+{
+    public class RegisterUserDto
+    {
+        public int? UserID { get; set; }
+        public string Name { get; set; }
+        public string GCMClientToken { get; set; }
+        public string OldAuthToken { get; set; }
+        public string AuthToken { get; set; }
+
+    }
+}
---
+++ WebService/SnapTown.WebService/Converters/UserConverter.cs
@@ -0,0 +1,31 @@
+﻿using SnapTown.DTO;
+using SnapTown.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace SnapTown.WebService.Converters
+{
+    public static class UserConverter
+    {
+        public static readonly Expression<Func<User, RegisterUserDto>> AsUserDto =
+              x => new RegisterUserDto
+              {
+                  UserID = x.UserID,
+                  Name = x.Name,
+                  AuthToken = x.AuthToken,
+                  GCMClientToken = x.GCMClientToken
+              };
+
+        public static readonly Expression<Func<RegisterUserDto, User>> AsUser =
+             x => new User()
+             {
+                 UserID = x.UserID.Value,
+                 Name = x.Name,
+                 AuthToken = x.AuthToken,
+                 GCMClientToken = x.GCMClientToken
+             };
+    }
+}
