6f0467d65281bd3eee799223c9f9037c011a70ed
RadoslavGatev <radoslavgatev@gmail.com>
2015-01-29 23:45:03 +0200

    Fixed a bug with the Authentication api controller.

---
+++ WebService/SnapTown.WebService/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using SnapTown.DAL;
+using SnapTown.DTO;
+using SnapTown.Models;
+using SnapTown.WebService.Converters;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SnapTown.WebService.Controllers
+{
+    [RoutePrefix("api/auth")]
+    public class Auth : ApiController
+    {
+        private IUnitOfWork unitOfWork { get; set; }
+        public Auth(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        [Route("")]
+        public int Get(RegisterUserDto registerUser)
+        {
+            //TODO verify facebook
+
+            //register
+            var user = UserConverter.AsUser.Compile().Invoke(registerUser);
+            this.unitOfWork.Users.Create(user);
+
+            this.unitOfWork.SaveChanges();
+
+            return user.UserID;
+        }
+
+        //[HttpPut]
+        [Route("")]
+        public void Post(RegisterUserDto registerUser)
+        {
+            //TODO verify facebook
+
+            var user = this.unitOfWork.Users.Get(u => u.UserID == registerUser.UserID &&
+                 u.AuthToken == registerUser.OldAuthToken);
+
+            user.AuthToken = registerUser.AuthToken;
+            this.unitOfWork.Users.Update(user);
+
+            this.unitOfWork.SaveChanges();
+        }
+    }
+}
